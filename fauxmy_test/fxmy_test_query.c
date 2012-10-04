92fb85a1f974cfe7afdb28c02f9e2aa5935a3ca7
Max Burke <maxburke@gmail.com>
2012-10-04 07:50:26 -0700

    Adding test for string literals, including string containing quotes (ie, '\''). This currently fails.

--- fauxmy_test/fxmy_test_query.c
+++ fauxmy_test/fxmy_test_query.c
@@ -2,12 +2,14 @@
 
 #include "fxmy_core.h"
 #include "fxmy_query.h"
+#include "fxmy_string.h"
 #include "fxmy_test.h"
 
 static const char *test_queries[] = {
     "SELECT option_name, option_value FROM wp_options WHERE autoload = 'yes'",
     "SELECT option_name, option_value FROM wp_options",
     "SELECT option_value FROM wp_options WHERE option_name = 'siteurl'",
+    "SELECT foo FROM bar WHERE baz = 'hello how are you\' today'",
     "DESCRIBE wp_users;"
 };
 
@@ -30,7 +32,8 @@ test_next_token(void)
     const char *expected0[] = { "SELECT", "option_name", ",", "option_value", "FROM", "wp_options", "WHERE", "autoload", "=", "'yes'" };
     const char *expected1[] = { "SELECT", "option_name", ",", "option_value", "FROM", "wp_options" };
     const char *expected2[] = { "SELECT", "option_value", "FROM", "wp_options", "WHERE", "option_name", "=", "'siteurl'" };
-    const char *expected3[] = { "DESCRIBE", "wp_users", ";" };
+    const char *expected3[] = { "SELECT", "foo", "FROM", "bar", "WHERE", "baz", "=", "'hello how are you\' today'" };
+    const char *expected4[] = { "DESCRIBE", "wp_users", ";" };
 
     struct expected_tokens_t expected_tokens[FXMY_ARRAY_COUNT(test_queries)];
 
@@ -41,6 +44,7 @@ test_next_token(void)
     INITIALIZE_EXPECTED_TOKENS(1);
     INITIALIZE_EXPECTED_TOKENS(2);
     INITIALIZE_EXPECTED_TOKENS(3);
+    INITIALIZE_EXPECTED_TOKENS(4);
     
     for (i = 0; i < FXMY_ARRAY_COUNT(expected_tokens); ++i)
     {
@@ -54,6 +58,7 @@ test_next_token(void)
         for (ii = 0; ii < num_tokens; ++ii)
         {
             begin = fxmy_next_token(&end, begin);
+            TEST((size_t)(end - begin) == strlen(expected_tokens[i].tokens[ii]));
             TEST(strncmp(begin, expected_tokens[i].tokens[ii], strlen(expected_tokens[i].tokens[ii])) == 0);
 
             begin = end;
