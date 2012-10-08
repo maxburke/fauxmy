304d9aced27874a1c401e033ebd16e124c402964
Max Burke <maxburke@gmail.com>
2012-10-08 01:19:16 -0700

    Worked a bit more on the rearrange_limit function. Documented the heck out of it. Currently not unit tested... yet.

--- fxmy_query.c
+++ fxmy_query.c
@@ -118,32 +118,128 @@ column_query_failed:
     return 0;
 }
 
-static void
-fxmy_rearrange_limit(fxmy_char *query)
+/*
+ * MySQL selects a certain number of rows by using a a "SELECT ... LIMIT x"
+ * query whereas MSSQL uses a "SELECT TOP x ..." style query. This function,
+ * fxmy_rearrange_limit, converts the former to the latter.
+ */
+static int
+fxmy_rearrange_limit(struct fxmy_connection_t *conn, fxmy_char *query)
 {
     const fxmy_char *limit_begin;
     const fxmy_char *limit_end;
     const fxmy_char *num_begin;
     const fxmy_char *num_end;
+    const fxmy_char *select_begin;
     const fxmy_char top[] = C("TOP  ");
+    fxmy_char *num_storage;
+    fxmy_char *next_token_after_select;
+    int shift_width;
+    int number_string_length;
+    size_t bytes_to_shift;
 
+    select_begin = NULL;
+    next_token_after_select = NULL;
     limit_begin = query;
 
+    /*
+     * This loop iterates through the query string one token at a time in
+     * order to identify the LIMIT clause.
+     */
     for (;;)
     {
         limit_begin = fxmy_fnext_token(&limit_end, limit_begin);
+
+        /*
+         * The "TOP x" bit is copied to the front of the query after the 
+         * SELECT but we first need to identify where the first token
+         * after the select begins.
+         */
+        if (!select_begin && fxmy_fstrnicmp(limit_begin, C("SELECT"), 6) == 0)
+        {
+            select_begin = limit_begin;
+        } 
+        else if (!next_token_after_select && select_begin) 
+        {
+            /*
+             * This code purposely casts away const as we need to write to
+             * this address further down the program. The limit_begin
+             * variable is otherwise const to prevent unintended writes
+             * from happening through it.
+             */
+            next_token_after_select = (fxmy_char *)limit_begin;
+        }
+
+        /*
+         * If we find the LIMIT clause, break out of the loop, setting the 
+         * beginning of our number string to be the end of the limit clause.
+         * The exact bounds of the number string are determined below.
+         */
         if (fxmy_fstrnicmp(limit_begin, C("LIMIT"), 5) == 0)
         {
+            num_begin = limit_end;
             break;
         }
+
         limit_begin = limit_end;
     }
 
-    limit_begin = fxmy_fstristr(query, C("LIMIT"));
-    memcpy((fxmy_char *)limit_begin, top, sizeof top - sizeof(fxmy_char));
+    /*
+     * If we can't find a limit_begin, bail.
+     */
+    if (!limit_begin)
+    {
+        conn->status = fxmy_get_status(FXMY_ERROR_DEFAULT);
+        return 1;
+    }
 
-    limit_begin = fxmy_fnext_token(&num_begin, limit_begin);
+    /*
+     * Determine the bounds and length of the number string.
+     */
     num_begin = fxmy_fnext_token(&num_end, num_begin);
+    number_string_length = num_end - num_begin;
+
+    /*
+     * Allocate a temporary buffer that we copy the number string into.
+     * This is allocated on the heap, as are all temporary buffers in
+     * the program, so that a rogue copy operation doesn't end up 
+     * trampling all over our stack. This buffer is allocated to include
+     * space for the string itself, a space character, and a null
+     * terminator.
+     */
+    num_storage = calloc(number_string_length + 2, sizeof(fxmy_char));
+    memmove(num_storage, num_begin, number_string_length);
+
+    /*
+     * The number string has a space appended as we need to separate
+     * that string from the next token in the stream.
+     */
+    num_storage[number_string_length] = C(' ');
+
+    shift_width = num_end - limit_begin;
+    bytes_to_shift = (limit_begin - next_token_after_select) * sizeof(fxmy_char);
+
+    /*
+     * Shift the string down to stomp over the LIMIT x clause.
+     */
+    memmove(next_token_after_select + shift_width, next_token_after_select, bytes_to_shift);
+
+    /*
+     * Copy in the "TOP  " string into our buffer.
+     */
+    memmove(next_token_after_select, top, (FXMY_ARRAY_COUNT(top) - 1) * sizeof(fxmy_char));
+
+    /*
+     * Copy in our number string last.
+     */
+    memmove(next_token_after_select + FXMY_ARRAY_COUNT(top) - 1, num_storage, (number_string_length + 1) * sizeof(fxmy_char));
+
+    /*
+     * At this point our num_storage buffer is no longer needed and can be
+     * returned to the general heap.
+     */
+    free(num_storage);
+    return 0;
 }
 
 int
@@ -171,6 +267,10 @@ fxmy_handle_query(struct fxmy_connection_t *conn, uint8_t *query_string, size_t 
     {
         return fxmy_describe(conn, query);
     }
+    else if (fxmy_stristr(query, "SHOW TABLES") != NULL)
+    {
+        __debugbreak();
+    }
     else
     {
         SQLHANDLE query_handle;
@@ -186,7 +286,8 @@ fxmy_handle_query(struct fxmy_connection_t *conn, uint8_t *query_string, size_t 
          */
 
         if (fxmy_fstristr(wide_query, C("LIMIT ")))
-            fxmy_rearrange_limit(wide_query);
+            if (fxmy_rearrange_limit(conn, wide_query))
+                return 0;
 
         VERIFY_ODBC(SQLAllocHandle(SQL_HANDLE_STMT, odbc->database_connection_handle, &query_handle), SQL_HANDLE_DBC, odbc->database_connection_handle);
         status = fxmy_verify_and_log_odbc(SQLExecDirect(query_handle, wide_query, SQL_NTS), SQL_HANDLE_STMT, query_handle);
